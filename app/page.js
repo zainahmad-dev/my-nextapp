e37c3d3959227f21a52a741df39327ec99b4b08d
Zain Ahmad <zainahmad4708@gmail.com>
2025-09-10 22:27:17 +0500

    minor changes

--- app/page.js
+++ app/page.js
@@ -1,5 +1,22 @@
 const Home = () => {
-  return<h1>Welcome to the zanu world</h1>
+
+  return(
+      <header>
+            <div className="thapa">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/about">About</a>
+                    </li>
+                    <li>
+                        <a href="/service">Service</a>
+                    </li>
+                   
+                </ul>
+            </nav>
+            </div>
+        </header>
+  )
 }
 
 export default Home;
---
+++ app/service/page.jsx
@@ -0,0 +1,12 @@
+
+const page = () => {
+  return (
+    <div>
+        <h1>
+            this is service page
+        </h1>
+    </div>
+  )
+}
+
+export default page
